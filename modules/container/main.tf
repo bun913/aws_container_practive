b5bd457c0d284e490235fe196104ae1fe9f6d1ea
bun913 <imanau03@gmail.com>
2021-12-07 06:40:17 +0900

    feat: add backend task definition

--- modules/container/main.tf
+++ modules/container/main.tf
@@ -43,3 +43,40 @@ resource "aws_iam_policy" "deploy_plicy" {
   name   = "ecs-deploy-role"
   policy = file("./files/ecs_iam_policy.json")
 }
+
+// CloudWatchLogs for backend
+resource "aws_cloudwatch_log_group" "backend" {
+  name              = "sbcntr-backend-log"
+  retention_in_days = 30
+}
+
+// ECS
+resource "aws_ecs_task_definition" "service" {
+  family                   = "sbcntr-backend-def"
+  cpu                      = 512
+  memory                   = 1024
+  requires_compatibilities = ["FARGATE"]
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = "${aws_ecr_repository.backend.repository_url}:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-region = "ap-northeast-1"
+          awslogs-group  = aws_cloudwatch_log_group.backend.id
+        }
+      }
+    }
+  ])
+}
+
